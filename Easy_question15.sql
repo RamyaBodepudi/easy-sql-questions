ef589b470b12b1771a6356596df64fd2834e2694
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 13:55:49 -0600

    Sql easy question 15 added

--- Easy_question15.sql
+++
@@ -1,3 +0,0 @@
-# 15. Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
-select first_name, last_name, birth_date from patients
-where height > 160 and weight > 70;
