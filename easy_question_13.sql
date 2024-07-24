608e063a79ac0df002e0f25502f1e335ab471fee
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 13:51:43 -0600

    Sql easy question 13 added

--- easy_question_13.sql
+++ easy_question_13.sql
@@ -1,3 +1,3 @@
-#13. Show the patient id and the total number of admissions 
+-- 13. Show the patient id and the total number of admissions 
 select patient_id, count(*) as Total_admissions  from admissions
 where patient_id = 579;
