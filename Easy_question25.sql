6a4a6921c7e8e084fb53a927bf47591f03357520
Ramya Bodepudi <ramya@Krishnas-MacBook-Pro-Personal.local>
2024-07-23 18:24:23 -0600

    Sql easy question 25 added

---
+++ Easy_question25.sql
@@ -0,0 +1,3 @@
+#25. Show the average unit price rounded to 2 decimal places, the total units in stock, total discontinued products from the products table.
+select round(avg(unit_price),2) as avg_price, sum(units_in_stock) as total_units_stock,
+Sum(discontinued) as total_discontinued from products;
