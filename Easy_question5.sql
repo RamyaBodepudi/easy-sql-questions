0a21f021f9a5e57f4eefb00a09d72c40bcdca7d2
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 00:45:02 -0600

    Sql easy question 5 added

--- Easy_question5.sql
+++
@@ -1,5 +0,0 @@
-#5. Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
-update patients 
-set allergies = 'NKA'
-
-where allergies is null;
