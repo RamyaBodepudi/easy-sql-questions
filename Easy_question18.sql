3c3985586bf4efc5c119ff752bda8a51bea47e1d
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-23 18:16:04 -0600

    Sql easy question 18 added

---
+++ Easy_question18.sql
@@ -0,0 +1,3 @@
+#18. Show all the contact_name, address, city of all customers which are not from 'Germany', 'Mexico', 'Spain'
+select contact_name, address, city from customers 
+where country not in ('Germany', 'Mexico', 'Spain');
