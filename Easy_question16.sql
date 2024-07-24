fd99d4e0bcb0789fea25d74634ebb17e0e90057f
Ramya Bodepudi <ramya@Krishnas-MacBook-Pro-Personal.local>
2024-07-23 18:07:03 -0600

    Sql easy question 16 added

---
+++ Easy_question16.sql
@@ -0,0 +1,3 @@
+#16. Write a query to find list of patients first_name, last_name, and allergies where allergies are not null and are from the city of 'Hamilton'
+select first_name, last_name, allergies from patients
+where allergies is not null and city = 'Hamilton';
