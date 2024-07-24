6943731feeac8f74f74ee8dd5449ffdec4b86283
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 00:51:53 -0600

    Sql easy question 8 added

--- easy_question_8.sql
+++ easy_question_8.sql
@@ -1,3 +1,3 @@
-#8. Show how many patients have a birth_date with 2010 as the birth year.
+-- 8. Show how many patients have a birth_date with 2010 as the birth year.
 select count(patient_id) as Total_patients from patients
 where year(birth_date) = 2010;
