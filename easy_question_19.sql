7e2469b9d67eaaa8f8dbc6eb6e377041d59b5f46
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 14:07:26 -0600

    Sql easy question 19 added

--- easy_question_19.sql
+++ easy_question_19.sql
@@ -1,3 +1,3 @@
-#19. Show order_date, shipped_date, customer_id, Freight of all orders placed on 2018 Feb 26
+-- 19. Show order_date, shipped_date, customer_id, Freight of all orders placed on 2018 Feb 26
 select order_date, shipped_date, customer_id, freight from orders
 where order_date = '2018-02-26';
