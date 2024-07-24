c72cbc4e1f329bddace8783a0fbd12dbfcbbd796
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-23 23:53:38 -0600

    Sql easy question 1 added

--- Easy_question1.sql
+++ Easy_question1.sql
@@ -1,3 +1,2 @@
-#1. Show first name, last name, and gender of patients whose gender is 'M'
-SELECT first_name, last_name, gender FROM patients
-Where gender = 'M';
+-- 1. Show first name, last name, and gender of patients whose gender is 'M'
+SELECT first_name, last_name, gender FROM patients Where gender = 'M';
