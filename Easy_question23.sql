1e8d2f60b806de163dbc92699107295ec58a7bf6
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 14:13:50 -0600

    Sql easy question 23 added

--- Easy_question23.sql
+++
@@ -1,3 +0,0 @@
-#23. Show the company_name, contact_name, fax number of all customers that has a fax number. (not null)
-select company_name, contact_name, fax from customers 
-where fax is not null;
