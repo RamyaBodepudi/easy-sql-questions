60bca2014291edd32f2215381c37ec25b544cd03
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-24 14:18:25 -0600

    Revert "Sql easy question"
    This reverts commit 081e894a68acd83590c11ef99a880a25e2ed4d01.

--- easy_question_25.sql
+++
@@ -1,3 +0,0 @@
-#-- 5. Show the average unit price rounded to 2 decimal places, the total units in stock, total discontinued products from the products table.
-select round(avg(unit_price),2) as avg_price, sum(units_in_stock) as total_units_stock,
-Sum(discontinued) as total_discontinued from products;
